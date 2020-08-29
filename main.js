f845ed39b14d4fb87abb812c3f62c774b9fd1512
V1000101 <1qazxcvbnmklp2@gmail.com>
2020-08-29 14:49:18 -0500

    typo

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ function Atom1Rename(){
 var atomprompt1=prompt("What would you like to name your atom?")
 document.getElementById("atom1").innerHTML=atomprompt1+" is a very small line of particles that is big enough to casue the formatiln of gravity. You can use this gravity and turn it into energy. Each atom produces 100E/s"
 document.getElementById("atom01").innerHTML="Form 1 "+atomprompt1+" for 100 particles"
-document.getElementById("atom0110").innerHTML="Form 10"+atomprompt1+" for 1,000 particles"
-document.getElementById("atom01100").innerHTML="Form 100"+atomprompt1+" for 10,000 particles"
+document.getElementById("atom0110").innerHTML="Form 10 "+atomprompt1+" for 1,000 particles"
+document.getElementById("atom01100").innerHTML="Form 100 "+atomprompt1+" for 10,000 particles"
 document.getElementById("atom001").innerHTML="Rename "+atomprompt1
 }
