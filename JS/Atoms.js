4b32a5fef9767f712bfc47d97f8b49fe369d222f
V1000101 <1qazxcvbnmklp2@gmail.com>
2020-08-31 19:00:43 -0500

    l

--- JS/Atoms.js
+++ JS/Atoms.js
@@ -1,22 +1,83 @@
+// Atom 1
 function formAtom1(qty){
-    if(gameData.OneDimeParticle>=gameData.OneDimeAtom1Cost*qty){
-        gameData.OneDimeAtom1+=1*qty
-        gameData.OneDimeParticle-=gameData.OneDimeAtom1Cost*qty
+    if(gameData.OneDimeParticle>=gameData.OneDimeAtom1Cost*qty*bMA){
+        gameData.OneDimeAtom1+=1*qty*bMA
+        gameData.OneDimeParticle-=gameData.OneDimeAtom1Cost*qty*bMA
     }
     else{
         alert("You need more particles to make that atom")
     }
-    document.getElementById("atomtotal").innerHTML="Total Atoms: "+gameData.OneDimeAtom1
-    document.getElementById("CMC").innerHTML="Particle Count: "+gameData.OneDimeParticle.toFixed(0) +"("+((gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toFixed(0)+")"
-    document.getElementById("CMC2").innerHTML="Particle Count: "+gameData.OneDimeParticle.toFixed(0) +"("+((gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toFixed(0)+")"
+    document.getElementById("atomtotal").innerHTML="Total Atoms: "+gameData.OneDimeAtom1.toExponential(1)
+    document.getElementById("CMC").innerHTML="Particle Count: "+gameData.OneDimeParticle.toExponential(1) +"("+((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toExponential(1)+")"
+    document.getElementById("CMC2").innerHTML="Particle Count: "+gameData.OneDimeParticle.toExponential(1) +"("+((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toExponential(1)+")"
 }
 
+var bMA=1
+function buyMultiOneAtom(){
+var a=prompt("Please enter a positive whole number.")
+if(a*1>=1){
+    bMA=(a*1).toFixed(0)
+    document.getElementById("atom01").innerHTML= "Form "+(1*bMA).toExponential(1)+" "+atomprompt1+" for "+(100*bMA).toExponential(1)+" particles"
+    document.getElementById("atom0110").innerHTML= "Form "+(10*bMA).toExponential(1)+" "+atomprompt1+" for "+(1000*bMA).toExponential(1)+" particles"
+    document.getElementById("atom01100").innerHTML= "Form "+(100*bMA).toExponential(1)+" "+atomprompt1+" for "+(10000*bMA).toExponential(1)+" particles"
+    document.getElementById("bmatom").innerHTML= "Buy Multi, Current Multi: "+(1*bMA).toExponential(1)
+}
+else{
+    alert("Please try again.")
+}
+}
+var atomprompt1="Atom 1"
 function Atom1Rename(){
-var atomprompt1=prompt("What would you like to name your atom?")
+atomprompt1=prompt("What would you like to name your atom?")
 document.getElementById("atom1").innerHTML=atomprompt1+" is a very small line of particles that is big enough to casue the formatiln of gravity. You can use this gravity and turn it into energy. Each atom produces 100E/s"
-document.getElementById("atom01").innerHTML="Form 1 "+atomprompt1+" for 100 particles"
-document.getElementById("atom0110").innerHTML="Form 10 "+atomprompt1+" for 1,000 particles"
-document.getElementById("atom01100").innerHTML="Form 100 "+atomprompt1+" for 10,000 particles"
+document.getElementById("atom01").innerHTML="Form "+(1*bMA).toExponential(1)+" "+atomprompt1+" for "+(100*bMA).toExponential(1)+" particles"
+document.getElementById("atom0110").innerHTML="Form "+(10*bMA).toExponential(1)+" "+atomprompt1+" for "+(1000*bMA).toExponential(1)+" particles"
+document.getElementById("atom01100").innerHTML="Form "+(100*bMA).toExponential(1)+" "+atomprompt1+" for "+(10000*bMA).toExponential(1)+" particles"
 document.getElementById("atom001").innerHTML="Rename "+atomprompt1
 }
+//Atom 2
 
+onclick=setInterval(formingAtom1, 100)
+function formingAtom1(){
+    if(((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle)<=gameData.OneDimeLength){
+    gameData.OneDimeAtom1+=1e9*gameData.OneDimeAtom2
+    document.getElementById("atomtotal").innerHTML="Total Atoms: "+gameData.OneDimeAtom1.toExponential(1)
+    }
+}
+
+function formAtom2(qty){
+    if(gameData.OneDimeParticle>=gameData.OneDimeAtom2Cost*qty*bMA2){
+        gameData.OneDimeAtom2+=1*qty*bMA2
+        gameData.OneDimeParticle-=gameData.OneDimeAtom2Cost*qty*bMA2
+    }
+    else{
+        alert("You need more particles to make that atom")
+    }
+    document.getElementById("atomtotal2").innerHTML="Total Atoms: "+gameData.OneDimeAtom2.toExponential(1)
+    document.getElementById("CMC").innerHTML="Particle Count: "+gameData.OneDimeParticle.toExponential(1) +"("+((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toExponential(1)+")"
+    document.getElementById("CMC2").innerHTML="Particle Count: "+gameData.OneDimeParticle.toExponential(1) +"("+((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toExponential(1)+")"
+}
+
+var bMA2=1
+function buyMultiOneAtom2(){
+var a=prompt("Please enter a positive whole number.")
+if(a*1>=1){
+    bMA2=(a*1).toFixed(0)
+    document.getElementById("atom02").innerHTML= "Form "+(1*bMA2).toExponential(1)+" "+atomprompt2+" for "+(1e12*bMA2).toExponential(1)+" particles"
+    document.getElementById("atom0210").innerHTML= "Form "+(10*bMA2).toExponential(1)+" "+atomprompt2+" for "+(1e13*bMA2).toExponential(1)+" particles"
+    document.getElementById("atom02100").innerHTML= "Form "+(100*bMA2).toExponential(1)+" "+atomprompt2+" for "+(1e14*bMA2).toExponential(1)+" particles"
+    document.getElementById("bmatom2").innerHTML= "Buy Multi, Current Multi: "+(1*bMA2).toExponential(1)
+}
+else{
+    alert("Please try again.")
+}
+}
+var atomprompt2="Atom 2"
+function Atom2Rename(){
+atomprompt2=prompt("What would you like to name your atom?")
+document.getElementById("atom2").innerHTML=atomprompt1+" is a very small line of particles that is big enough to casue the formatiln of gravity. You can use this gravity and turn it into energy. Each atom produces 100E/s"
+document.getElementById("atom02").innerHTML="Form "+(1*bMA2).toExponential(1)+" "+atomprompt2+" for "+(1e12*bMA2).toExponential(1)+" particles"
+document.getElementById("atom0210").innerHTML="Form "+(10*bMA2).toExponential(1)+" "+atomprompt2+" for "+(1e13*bMA2).toExponential(1)+" particles"
+document.getElementById("atom02100").innerHTML="Form "+(100*bMA2).toExponential(1)+" "+atomprompt2+" for "+(1e14*bMA2).toExponential(1)+" particles"
+document.getElementById("atom002").innerHTML="Rename "+atomprompt2
+}
--- JS/Energy-Matter-DarkMatter.js
+++ JS/Energy-Matter-DarkMatter.js
@@ -1,14 +1,15 @@
+//Energy
 onclick=setInterval(energyClock, 100)
 function energyClock(){
     gameData.OneDimeEnergy+=10*gameData.OneDimeGodAura+((gameData.OneDimeAtom1*100)*gameData.OneDimeGodAura)
-    document.getElementById("EC").innerHTML="Energy Count: "+gameData.OneDimeEnergy+". You gain "+(10*10*gameData.OneDimeGodAura+((gameData.OneDimeAtom1*100)*gameData.OneDimeGodAura)).toFixed(1)+"E/s"
-    document.getElementById("PercentTill2D").innerHTML="You are currently "+((Math.log10(gameData.OneDimeParticle+1+(gameData.OneDimeAtom1*100))/Math.log10(1.7e308))*100).toFixed(3)+"% until you can access the second dimension!"
+    document.getElementById("EC").innerHTML="Energy Count: "+gameData.OneDimeEnergy.toExponential(1)+". You gain "+(10*10*gameData.OneDimeGodAura+((gameData.OneDimeAtom1*100)*gameData.OneDimeGodAura)).toExponential(1)+"E/s"
+    document.getElementById("PercentTill2D").innerHTML="You are currently "+((Math.log10(gameData.OneDimeParticle+1+(gameData.OneDimeAtom1*100)+(gameData.OneDimeAtom2*1e12))/Math.log10(1.7e308))*100).toFixed(3)+"% until you can access the second dimension!"
 }
-
+//fill bar
 onclick=setInterval(move, 10)
   function move(){
    var length=gameData.OneDimeLength
-   var matter=gameData.OneDimeParticle+(gameData.OneDimeAtom1*100)
+   var matter=gameData.OneDimeParticle+(gameData.OneDimeAtom1*100)+(gameData.OneDimeAtom2*1e12)
     if(matter/length<=1){
       var elem = document.getElementById("myBar");
           width =((matter/length)*1000).toFixed(2)
@@ -17,14 +18,15 @@ onclick=setInterval(move, 10)
           elem.innerHTML = widthbartext + "%";
         }
     }
+//matter
 function gainOneDimeMatter(qty){
     if(gameData.OneDimeEnergy>=gameData.OneDimeParticleCost*qty*bMM){
         if(((gameData.OneDimeAtom1*100)+qty*bMM+gameData.OneDimeParticle)<=gameData.OneDimeLength){
     gameData.OneDimeParticle+=gameData.OneDimeParticleGain*gameData.OneDimeGodPower*qty*bMM
     gameData.OneDimeEnergy-=gameData.OneDimeParticleCost*qty*bMM
-    document.getElementById("CMC").innerHTML="Particle Count: "+gameData.OneDimeParticle.toFixed(0) +"("+((gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toFixed(0)+")"
-    document.getElementById("CMC2").innerHTML="Particle Count: "+gameData.OneDimeParticle.toFixed(0) +"("+((gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toFixed(0)+")"
-    document.getElementById("EC").innerHTML="Energy Count: "+gameData.OneDimeEnergy+". You gain "+((gameData.OneDimeGodAura*10*10)+gameData.OneDimeAtom1*100*gameData.OneDimeGodAura).toFixed(1)+"E/s"
+    document.getElementById("CMC").innerHTML="Particle Count: "+gameData.OneDimeParticle.toExponential(1) +"("+((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toExponential(1)+")"
+    document.getElementById("CMC2").innerHTML="Particle Count: "+gameData.OneDimeParticle.toExponential(1) +"("+((gameData.OneDimeAtom2*1e12)+(gameData.OneDimeAtom1*100)+gameData.OneDimeParticle).toExponential(1)+")"
+    document.getElementById("EC").innerHTML="Energy Count: "+gameData.OneDimeEnergy.toExponential(1)+". You gain "+((gameData.OneDimeGodAura*10*10)+gameData.OneDimeAtom1*100*gameData.OneDimeGodAura).toExponential(1)+"E/s"
         }
         else if(((gameData.OneDimeAtom1*100)+qty*bMM+gameData.OneDimeParticle)>=gameData.OneDimeLength){
             alert("You need to make the universe longer for you to produce more matter")
@@ -40,19 +42,19 @@ function buyMultiOneDimeMatter(){
 var a=prompt("Please enter a positive whole number.")
 if(a*1>=1){
     bMM=(a*1).toFixed(0)
-    document.getElementById("1DParticle").innerHTML= (1*bMM).toFixed(0)+" Particle for "+(100*bMM).toFixed(0)+" energy"
-    document.getElementById("1DParticle10").innerHTML= (10*bMM).toFixed(0)+" Particle for "+(1000*bMM).toFixed(0)+" energy"
-    document.getElementById("1DParticle100").innerHTML= (100*bMM).toFixed(0)+" Particle for "+(10000*bMM).toFixed(0)+" energy"
-    document.getElementById("1DbuyMultiParticle").innerHTML= "Buy Multi, Current Multi: "+bMM
+    document.getElementById("1DParticle").innerHTML= (1*bMM).toExponential(1)+" Particle for "+(100*bMM).toExponential(1)+" energy"
+    document.getElementById("1DParticle10").innerHTML= (10*bMM).toExponential(1)+" Particle for "+(1000*bMM).toExponential(1)+" energy"
+    document.getElementById("1DParticle100").innerHTML= (100*bMM).toExponential(1)+" Particle for "+(10000*bMM).toExponential(1)+" energy"
+    document.getElementById("1DbuyMultiParticle").innerHTML= "Buy Multi, Current Multi: "+(1*bMM).toExponential(1)
 }
 else{
     alert("Please try again.")
 }
 }
-
+//Dark Matter
 onclick=setInterval(function(){if(gameData.OneDimeDM>1){
     gameData.OneDimeLength+=gameData.OneDimeDM
-    document.getElementById("LoTU").innerHTML="Length of the Universe: "+gameData.OneDimeLength.toFixed(1) +" Meters. You gain "+(gameData.OneDimeDM).toFixed(1)+"M/s"
+    document.getElementById("LoTU").innerHTML="Length of the Universe: "+gameData.OneDimeLength.toExponential(1) +" Meters. You gain "+(gameData.OneDimeDM).toExponential(1)+"M/s"
     }
 },1000)
 
@@ -64,18 +66,18 @@ function gainOneDimeDMatter(qty){
     else if(gameData.OneDimeEnergy<gameData.OneDimeDMCost*qty*bMDM){
         alert("You need more energy to make dark matter")
     }
-    document.getElementById("dmtxt").innerHTML="Dark Matter, its what makes your universe expand. But there isn't enough. How about you make some!<br>Total Dark Matter: "+gameData.OneDimeDM.toFixed(1)
+    document.getElementById("dmtxt").innerHTML="Dark Matter, its what makes your universe expand. But there isn't enough. How about you make some!<br>Total Dark Matter: "+gameData.OneDimeDM.toExponential(1)
 }
 
 var bMDM=1
 function buyMultiOneDimeDM(){
 var a=prompt("Please enter a positive whole number.")
 if(a*1>=1){
-    bMBM=(a*1).toFixed(0)
-    document.getElementById("1DDP").innerHTML= (bMBM/10).toFixed(0)+" Dark Matter for "+(100*bMBM).toFixed(0)+" energy"
-    document.getElementById("1DDP10").innerHTML= (1*bMBM).toFixed(0)+" Dark Matter for "+(1000*bMBM).toFixed(0)+" energy"
-    document.getElementById("1DDP100").innerHTML= (10*bMBM).toFixed(0)+" Dark Matter for "+(10000*bMBM).toFixed(0)+" energy"
-    document.getElementById("1DbuyMultiDm").innerHTML= "Buy Multi, Current Multi: "+bMBM+"NOTE: THIS DOES NOT WORK QUITE YET"
+    bMDM=(a*1).toFixed(0)
+    document.getElementById("1DDP").innerHTML= (0.1*bMDM).toExponential(1)+" Dark Matter for "+(100*bMDM).toExponential(1)+" energy"
+    document.getElementById("1DDP10").innerHTML= (1*bMDM).toExponential(1)+" Dark Matter for "+(1000*bMDM).toExponential(1)+" energy"
+    document.getElementById("1DDP100").innerHTML= (10*bMDM).toExponential(1)+" Dark Matter for "+(10000*bMDM).toExponential(1)+" energy"
+    document.getElementById("1DbuyMultiDm").innerHTML= "Buy Multi, Current Multi: "+(1*bMDM).toExponential(1)
 }
 else{
     alert("Please try again.")
